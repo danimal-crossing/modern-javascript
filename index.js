87bc34f69a406e06c544eba297ce596836c12eb2
dani <dani.zaghian@gmail.com>
2020-06-25 13:04:41 -0700

    adds webpack dev server

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var math = require('mathjs');
 var dayBegin = moment().startOf('day').fromNow();
 console.log(dayBegin);
 console.log(math.sqrt(-4));
-var name = "Bob";
+var name = "James";
 var time = "today";
 console.log(`Hello ${name} how are you ${time}?`);
