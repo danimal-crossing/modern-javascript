d6437acc5bf7af116620665e28aef5347706f836
dani <dani.zaghian@gmail.com>
2020-06-25 11:20:45 -0700

    adds webpack + bable

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* global moment */
-var moment = require('moment');
+import moment from 'moment';
 var math = require('mathjs');
 
 var dayBegin = moment().startOf('day').fromNow();
 console.log(dayBegin);
-
 console.log(math.sqrt(-4));
+var name = "Bob";
+var time = "today";
+console.log(`Hello ${name} how are you ${time}?`);
